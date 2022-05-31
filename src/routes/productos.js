84bfd92ea124bc6ecbb6f23c08dcf7a1e1351616
Pablo <delatorre.pablodaniel@gmail.com>
2022-05-31 01:34:52 -0300

    avances

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,7 +1,25 @@
 import { Router } from "express";
+import Api from '../apiClass'
 
 const router = Router()
+const api = new Api("/dataBase/productos.json")
 
+router.get('/', async (req,res) => {
+    const products = await api.findAll()
+    res.json(products)
+})
+
+router.get('/:id', async (req,res) => {
+    const {id} = req.params
+    const product = await api.findById(id)
+    res.json(product)
+})
+
+router.post('/', async (req,res) => {
+    const obj = req.body
+    const product = await api.create(obj)
+    res.json(product)
+})
 
 
 
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
-
+import productosRoutes from './routes/productos'
+import carritoRoutes from './routes/carrito'
 const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
 
+app.use('/productos',productosRoutes)
+app.use('/carrito',carritoRoutes)
 
 
 const PORT = 8080
